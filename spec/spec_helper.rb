6686e6120e22bf1f5dd278d0b26a55e002aed3e4
Scott Taylor <scott@railsnewbie.com>
2010-02-26 16:55:06 -0500

    Move helpers into spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,33 @@
 require File.dirname(__FILE__) + "/../lib/action_mailer_logger"
+
+ActionMailer::Base.template_root = File.dirname(__FILE__) + "/templates"
+ActionMailer::Base.delivery_method = :test
+
+class UserMailer < ActionMailer::Base
+  def signup
+    from        "foo@example.com"
+    recipients  "bar@example.com"
+    subject     "TEST"
+    sent_on     Time.now.utc
+  end
+end
+
+require "active_record"
+
+class MailerLogger < ActiveRecord::Base
+  serialize :mailer
+end
+
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => ':memory:'
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define do
+  create_table :mailer_loggers, :force => true do |t|
+    t.string :class_name
+    t.string :method_name
+    t.text   :mailer
+    t.boolean :delivered
+    t.timestamps
+  end
+end
+
